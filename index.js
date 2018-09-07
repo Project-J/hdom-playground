e84754ae1ee51fa558b2d3fda92ca53d21062e7c
Dan Peddle <dan@iamfy.co>
2018-09-07 14:51:06 +0200

    base

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const hdom = require('hdom');
+const ourModule = require('./thing');
+
+ourModule.sayHello();
+
+console.log(hdom);
+
+
+
+console.log('hi there');
+console.log('another message');
+console.error('ohno!');
+
