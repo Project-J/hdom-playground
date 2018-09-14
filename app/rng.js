0e689d14a0d1c955c4f6244891f381367065d282
Dan Peddle <dan@iamfy.co>
2018-09-14 15:19:49 +0200

    silly state

--- app/rng.js
+++ app/rng.js
@@ -1,14 +1,14 @@
-function randInt(n){
-  return Math.floor(Math.random() * n);
+function randInt(n) {
+    return Math.floor(Math.random() * n);
 }
 
 function nRandoms(n, len) {
-  let result = Array(len).fill('');
-  return result.map(randInt.bind(null, n))
+    let result = Array(len).fill("");
+    return result.map(randInt.bind(null, n));
 }
 
 
 module.exports = {
-  randInt,
-  nRandoms
-}
+    randInt,
+    nRandoms
+};
--- index.js
+++ index.js
@@ -1,28 +1,71 @@
-'use strict';
-let hdom = require('@thi.ng/hdom');
-let rng = require('./app/rng');
+"use strict";
+let hdom = require("@thi.ng/hdom");
+let rng = require("./app/rng");
 
-let toChart = [0,1,2,3,1,8,10];
-
-
-function chart(_, toChart){
-
-    let numEls = toChart.length;
-    let elWidth = 100 / numEls;
-    let max = Math.max.apply(Math, toChart);
-
-    return ["section.chart",
-            toChart.map(function(v){
-              return ["div", {style: {
-                height: `${(v/max)*100}%`,
-                width: `${elWidth}%`}}];
-            })];
+function other({nums}) {
+    return [
+        "code", 
+        nums.reduce(
+            (acc, val) => acc + val, 
+            0)
+    ];
 }
 
-function app(){
-  return ["section", chart(null, rng.nRandoms(10,1000))]
+
+function nestedThing({some}) {
+    return ["p.other", some, other];
+}
+
+
+function status({nums}){
+    return ["h1.big", "loads of " + nums.length, nestedThing];
+}
+
+function chart({nums}) {
+    let numEls = nums.length;
+    let elWidth = 100 / numEls;
+    let max = Math.max.apply(Math, nums);
+    
+    return () => 
+        ["section.chart",
+            nums.map(function(v) {
+                return ["div", {
+                    style: {
+                        height: `${(v/max)*100}%`,
+                        width: `${elWidth}%`
+                    }
+                }];
+            }),
+            status
+        ];
+}
+
+
+function app() {
+    return () => ["section", chart];
 }
 
 
 // start update loop (browser only, see diagram below)
-hdom.start(app(), { root: document.body });
+
+const ctx = {
+    nums: rng.nRandoms(10, 150),
+    some: "thing",
+    data: {}
+};
+
+let numcols = 25;
+
+function update(){
+    let next = rng.nRandoms(10, numcols).sort();
+    ctx.nums = next.concat(rng.nRandoms(10, numcols).sort().reverse());
+}
+
+setInterval(update, 1000);
+
+hdom.start(
+    app(), 
+    { 
+        root: document.body, 
+        ctx
+    });
